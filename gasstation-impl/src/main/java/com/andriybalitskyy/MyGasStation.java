27fd373e3db85046234c0d7bb005b743375af6c8
Andriy Balitskyy <andrewincontact@gmail.com>
2022-08-10 09:38:22 +0300

    Implement GasStation interface in non thread-safe mode

--- gasstation-impl/src/main/java/com/andriybalitskyy/MyGasStation.java
+++ gasstation-impl/src/main/java/com/andriybalitskyy/MyGasStation.java
@@ -1,6 +1,10 @@
 package com.andriybalitskyy;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import net.bigpoint.assessment.gasstation.GasPump;
 import net.bigpoint.assessment.gasstation.GasStation;
@@ -9,51 +13,73 @@ import net.bigpoint.assessment.gasstation.exceptions.GasTooExpensiveException;
 import net.bigpoint.assessment.gasstation.exceptions.NotEnoughGasException;
 
 public class MyGasStation implements GasStation {
+	private List<GasPump> gasPumps = new ArrayList();
+	private Map<GasType, Double> prices = new HashMap();
+	private double revenue = 0;
+	private int NotEnoughGasCount = 0;
+	private int GasTooExpensiveCount = 0;
+	private int saleCount = 0;
 
 	public void addGasPump(GasPump pump) {
-		// TODO Auto-generated method stub
-
+		gasPumps.add(pump);
 	}
 
 	public Collection<GasPump> getGasPumps() {
-		// TODO Auto-generated method stub
-		return null;
+		return gasPumps;
 	}
 
 	public double buyGas(GasType type, double amountInLiters, double maxPricePerLiter)
 			throws NotEnoughGasException, GasTooExpensiveException {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		Boolean isNotEnoughGas = false;
+		
+		if(getPrice(type) > maxPricePerLiter) {
+			GasTooExpensiveCount++;
+			throw new GasTooExpensiveException();
+		}
+		
+		for(GasPump gasPump : gasPumps) {
+			if(gasPump.getGasType().equals(type)) {
+				if(gasPump.getRemainingAmount() >= amountInLiters) {
+					gasPump.pumpGas(amountInLiters);
+					revenue += amountInLiters * (maxPricePerLiter - getPrice(type));
+					saleCount++;
+				} else {
+					isNotEnoughGas = true;
+				}
+			}
+		}
+		
+		if(isNotEnoughGas) {
+			NotEnoughGasCount++;
+			throw new NotEnoughGasException();
+		}
+		
+		return maxPricePerLiter * amountInLiters;
 	}
 
 	public double getRevenue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return revenue;
 	}
 
 	public int getNumberOfSales() {
-		// TODO Auto-generated method stub
-		return 0;
+		return saleCount;
 	}
 
 	public int getNumberOfCancellationsNoGas() {
-		// TODO Auto-generated method stub
-		return 0;
+		return NotEnoughGasCount;
 	}
 
 	public int getNumberOfCancellationsTooExpensive() {
-		// TODO Auto-generated method stub
-		return 0;
+		return GasTooExpensiveCount;
 	}
 
 	public double getPrice(GasType type) {
-		// TODO Auto-generated method stub
-		return 0;
+		return prices.get(type);
 	}
 
 	public void setPrice(GasType type, double price) {
-		// TODO Auto-generated method stub
-
+		prices.put(type, price);
 	}
 
 }
--- gasstation-impl/src/main/java/com/andriybalitskyy/MyGasStationDemo.java
+++ gasstation-impl/src/main/java/com/andriybalitskyy/MyGasStationDemo.java
@@ -1,9 +1,44 @@
 package com.andriybalitskyy;
 
+import net.bigpoint.assessment.gasstation.GasPump;
+import net.bigpoint.assessment.gasstation.GasType;
+import net.bigpoint.assessment.gasstation.exceptions.GasTooExpensiveException;
+import net.bigpoint.assessment.gasstation.exceptions.NotEnoughGasException;
+
 public class MyGasStationDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NotEnoughGasException, GasTooExpensiveException {
 		MyGasStation myGasStation = new MyGasStation();
+		myGasStation.addGasPump(new GasPump(GasType.DIESEL, 80));
+		myGasStation.addGasPump(new GasPump(GasType.REGULAR, 40));
+		myGasStation.addGasPump(new GasPump(GasType.SUPER, 40));
+		
+		myGasStation.setPrice(GasType.DIESEL, 55);
+		myGasStation.setPrice(GasType.REGULAR, 42);
+		myGasStation.setPrice(GasType.SUPER, 48);
+
+		int countSalesBeforeOpening = myGasStation.getNumberOfSales();
+		System.out.println("countSalesBeforeOpening : " + countSalesBeforeOpening);
+		
+		myGasStation.buyGas(GasType.REGULAR, 10, 45);
+		myGasStation.buyGas(GasType.DIESEL, 10, 60);
+		myGasStation.buyGas(GasType.DIESEL, 10, 62);
+		myGasStation.buyGas(GasType.SUPER, 10, 48);
+//		myGasStation.buyGas(GasType.REGULAR, 10, 40);
+//		myGasStation.buyGas(GasType.REGULAR, 50, 45);
+		myGasStation.buyGas(GasType.DIESEL, 10, 70);
+		myGasStation.buyGas(GasType.REGULAR, 10, 45);
+		myGasStation.buyGas(GasType.DIESEL, 10, 60);
+		myGasStation.buyGas(GasType.DIESEL, 10, 62);
+		myGasStation.buyGas(GasType.SUPER, 10, 48);
+//		myGasStation.buyGas(GasType.REGULAR, 10, 40);
+//		myGasStation.buyGas(GasType.REGULAR, 50, 45);
+		myGasStation.buyGas(GasType.DIESEL, 10, 70);
+		
+		
+		System.out.println("NotEnoughGasCount " + myGasStation.getNumberOfCancellationsNoGas());
+		System.out.println("GasTooExpensiveCount " + myGasStation.getNumberOfCancellationsTooExpensive());
+		System.out.println("revenue " + myGasStation.getRevenue());
 	}
 
 }
